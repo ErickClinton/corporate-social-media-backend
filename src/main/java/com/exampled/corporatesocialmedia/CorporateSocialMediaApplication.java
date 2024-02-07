4ea0484919be64d44e4d17ca4bcfc4cc6bc06621
Erick Clinton <erickleite2009321@gmail.com>
2024-02-06 21:28:09 -0300

    base structure for starting the project

---
+++ src/main/java/com/exampled/corporatesocialmedia/CorporateSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.exampled.corporatesocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateSocialMediaApplication.class, args);
+	}
+
+}
