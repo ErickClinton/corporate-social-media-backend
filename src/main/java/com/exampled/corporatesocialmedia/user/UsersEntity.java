836394c2e082d8bb0ba2cb21988764f478cdfe7b
Erick Clinton <erickleite2009321@gmail.com>
2024-02-12 10:03:36 -0300

    refactor: changing id type

--- src/main/java/com/exampled/corporatesocialmedia/user/UsersEntity.java
+++ src/main/java/com/exampled/corporatesocialmedia/user/UsersEntity.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 public class UsersEntity{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private long id;
 
     @NotNull
