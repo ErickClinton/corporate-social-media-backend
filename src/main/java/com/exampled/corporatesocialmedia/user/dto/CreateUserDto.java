8fb269f18c2644e418b61cf79acee5b7a1c9546d
Erick Clinton <erickleite2009321@gmail.com>
2024-02-12 07:58:24 -0300

    fix: rename file

--- src/main/java/com/exampled/corporatesocialmedia/user/dto/CreateUserDto.java
+++
@@ -1,4 +0,0 @@
-package com.exampled.corporatesocialmedia.user.dto;
-
-public record CreateUserDto(String name, String email, String password) {
-}
---
+++ src/main/java/com/exampled/corporatesocialmedia/user/dto/UserDto.java
@@ -0,0 +1,4 @@
+package com.exampled.corporatesocialmedia.user.dto;
+
+public record UserDto(String name, String email, String password) {
+}
