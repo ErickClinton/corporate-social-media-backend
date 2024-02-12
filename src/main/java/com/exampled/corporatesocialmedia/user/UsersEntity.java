aaef5bfa6e51437314be2acb119c78b5bc0aba6b
Erick Clinton <erickleite2009321@gmail.com>
2024-02-12 11:06:08 -0300

    feat: creating a security route

--- src/main/java/com/exampled/corporatesocialmedia/user/UsersEntity.java
+++ src/main/java/com/exampled/corporatesocialmedia/user/UsersEntity.java
@@ -1,14 +1,17 @@
 package com.exampled.corporatesocialmedia.user;
 
+import com.exampled.corporatesocialmedia.enums.UserRoleEnum;
 import com.exampled.corporatesocialmedia.user.dto.UserDto;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity(name="users")
@@ -16,7 +19,8 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UsersEntity{
+@EqualsAndHashCode(of = "id")
+public class UsersEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -33,10 +37,44 @@ public class UsersEntity{
     @NotNull
     private String password;
 
+    @NotNull
+    private UserRoleEnum role;
+
     public void user(UserDto user){
         this.name = user.name();
         this.email = user.email();
         this.password = user.password();
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRoleEnum.Admin) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_MANAGER"),new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
+        else if(this.role == UserRoleEnum.Manager) return List.of(new SimpleGrantedAuthority("ROLE_MANAGER"),new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/exampled/corporatesocialmedia/user/UsersRepository.java
+++ src/main/java/com/exampled/corporatesocialmedia/user/UsersRepository.java
@@ -1,6 +1,9 @@
 package com.exampled.corporatesocialmedia.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsersRepository extends JpaRepository<UsersEntity,Long> {
+
+    UserDetails findByEmail(String email);
 }
