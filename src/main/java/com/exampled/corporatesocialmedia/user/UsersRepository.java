baf7ea907ff43ac6155896e4749141573e77e829
Erick Clinton <erickleite2009321@gmail.com>
2024-02-12 11:31:01 -0300

    fix: creating a deleteById with uuid

--- src/main/java/com/exampled/corporatesocialmedia/user/UsersRepository.java
+++ src/main/java/com/exampled/corporatesocialmedia/user/UsersRepository.java
@@ -3,7 +3,13 @@ package com.exampled.corporatesocialmedia.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface UsersRepository extends JpaRepository<UsersEntity,Long> {
 
     UserDetails findByEmail(String email);
+    UsersEntity findById(UUID id);
+    void deleteById(UUID id);
+
 }
